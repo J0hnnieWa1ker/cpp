bc6b9f329c1e735adeeed4e248f157031fede409
Jonathan Walker <jonathen@walker.ninja>
2015-11-07 00:08:08 -0800

    modified:   hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,22 @@
+
+//
+// This is example code from Chapter 3.1 "Input" of
+// "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
+//
+
 #include "std_lib_facilities.h"
 
-int main()             // C++ programs start by executing the function main
+//------------------------------------------------------------------------------
+
+// read and write a first name
+int main()
 {
-          cout << "Hello, World!\n";      // output “Hello, World!”
-          return 0;
+    cout << "Please enter your first name (followed by 'enter'):\n";
+    string first_name;    // first_name is a variable of type string
+    cin >> first_name;    // read characters into first_name
+    cout << "Hello, " << first_name << "!\n";
+    cout << "first_name" << " is " << first_name << "\n";
 }
 
+//------------------------------------------------------------------------------
 
