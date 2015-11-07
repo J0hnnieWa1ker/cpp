dc48e62158789d7faca36835921711dcc2179bbd
Jonathan Walker <jonathen@walker.ninja>
2015-11-07 00:14:26 -0800

    :	modified:   hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -7,15 +7,14 @@
 #include "std_lib_facilities.h"
 
 //------------------------------------------------------------------------------
-
-// read and write a first name
 int main()
 {
-    cout << "Please enter your first name (followed by 'enter'):\n";
-    string first_name;    // first_name is a variable of type string
-    cin >> first_name;    // read characters into first_name
-    cout << "Hello, " << first_name << "!\n";
-    cout << "first_name" << " is " << first_name << "\n";
+          cout << "Please enter your first name and age\n";
+          string first_name;          // string variable
+          int age;                          // integer variable
+          cin >> first_name;         // read a string
+          cin >> age;                    // read an integer
+          cout << "Hello, " << first_name << " (age " << age << ")\n";
 }
 
 //------------------------------------------------------------------------------
