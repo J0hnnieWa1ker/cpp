d51a47aa125ad0f7572a13852e6d7f5fb99cc846
Jonathan Walker <jonathen@walker.ninja>
2015-11-07 00:28:07 -0800

    concatenate 2 string inputs

--- hello.cpp
+++ hello.cpp
@@ -9,17 +9,12 @@
 //------------------------------------------------------------------------------
 int main()
 {
-          cout << "Please enter a floating-point value: ";
-          double n;
-          cin >> n;
-          cout << "n == " << n
-                    << "\nn+1 == " << n+1
-                    << "\nthree times n == " << 3*n
-                    << "\ntwice n == " << n+n
-                    << "\nn squared == " << n*n
-                    << "\nhalf of n == " << n/2
-                    << "\nsquare root of n == " << sqrt(n)
-                    << '\n';  // another name for newline (“end of line”) in output
+cout << "Please enter your first and second names\n";
+          string first;
+          string second;
+          cin >> first >> second;                         // read two strings
+          string name = first + ' ' + second;       // concatenate strings
+          cout << "Hello, " << name << '\n';
 }
 //------------------------------------------------------------------------------
 
