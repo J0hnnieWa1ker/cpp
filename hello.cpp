8315a44af8dd7de3012f743cf2bc53b80f4bc18b
Jonathan Walker <jonathen@walker.ninja>
2015-11-07 00:31:34 -0800

    added some if statements

--- hello.cpp
+++ hello.cpp
@@ -9,12 +9,16 @@
 //------------------------------------------------------------------------------
 int main()
 {
-cout << "Please enter your first and second names\n";
+          cout << "Please enter two names\n";
           string first;
           string second;
-          cin >> first >> second;                         // read two strings
-          string name = first + ' ' + second;       // concatenate strings
-          cout << "Hello, " << name << '\n';
+          cin >> first >> second;           // read two strings
+          if (first == second) cout << "that's the same name twice\n";
+          if (first < second)
+                    cout << first << " is alphabetically before " << second <<'\n';
+          if (first > second)
+                    cout << first << " is alphabetically after " << second <<'\n';
 }
+
 //------------------------------------------------------------------------------
 
