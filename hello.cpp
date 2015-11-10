1f9a9e4248a5c335db218ff1400d936355998b37
Jonathan Walker <jonathan@walker.ninja>
2015-11-09 23:13:14 -0800

    4.4.2.1 while-statements

--- hello.cpp
+++ hello.cpp
@@ -1,30 +1,22 @@
 
 //
-// This is example code from 4.4.1.2 switch-statements
+// This is example code from 4.4.2.1 while-statements
 // "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
 //
 
 #include "std_lib_facilities.h"
 //------------------------------------------------------------------------------
+int square(int x)    // return the square of x
+{
+          return x*x;
+}
 int main()
 {
- constexpr double cm_per_inch = 2.54;     // number of centimeters in
-                                                                              // an inch
-          double length = 1;                                     // length in inches or
-                                                                              // centimeters
-          char unit = 'a';
-          cout<< "Please enter a length followed by a unit (c or i):\n";
-          cin >> length >> unit;
-          switch (unit) {
-          case 'i':
-                    cout << length << "in == " << cm_per_inch*length << "cm\n";
-                    break;
-          case 'c':
-                    cout << length << "cm == " << length/cm_per_inch << "in\n";
-                    break;
-          default:
-                    cout << "Sorry, I don't know a unit called '" << unit << "'\n";
-                    break;
-          }
+         int i = 0;           // start from 0
+         while(i<200) {
+                   cout << i << '\t' << square(i) << '\n';
+                   ++i;        // increment i (that is, i becomes i+1)
+         }
 }
+
 
