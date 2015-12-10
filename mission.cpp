89c5e866778f0db7241875e3fb3306b4e512cb11
Jonathan Walker <jon@walker.ninja>
2015-12-09 23:09:57 -0800

    Adding mission statement program

---
+++ mission.cpp
@@ -0,0 +1,13 @@
+/*
+  Jonathan Walker
+  Jonathan@walker.ninja
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string const MissionStatement = "Personally view a piece of my fine art on the wall of every museum and significant art collector";
+    cout << MissionStatement << endl;
+    return 0;
+}
